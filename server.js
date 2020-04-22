ef59fbca1e7329b0a904440ec424b295ca028d79
Rémi Lécuyer <remil@connectngo.com>
2020-04-22 13:03:35 -0400

    update room

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const socketIO = require("socket.io");
 const path = require("path");
 
 // Configuration
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 const INDEX = path.join(__dirname, 'index.html');
 
 // Start server
@@ -19,10 +19,21 @@ const io = socketIO(server);
 io.on("connection", function(socket) {
   // Register "join" events, requested by a connected client
   socket.on("join", function (room) {
+    console.log(room, 'join')
     // join channel provided by client
     socket.join(room)
     // Register "image" events, sent by the client
+    var clients = io.sockets.adapter.rooms[room];
+    console.log(clients, 'number of client in the room'+room)
+
+    var srvSockets = io.sockets.sockets;
+    console.log(Object.keys(srvSockets).length, 'Count all clients connected to server');
+
+    var nspSockets = io.of('/chat').sockets;
+    console.log(Object.keys(nspSockets).length, 'Count all clients connected to namespace chat')
+
     socket.on("image", function(msg) {
+      console.log(msg, 'image')
       // Broadcast the "image" event to all other clients in the room
       socket.broadcast.to(room).emit("image", msg);
     });
